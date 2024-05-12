fc83e393f117355bbc320326ff7b347ee6a09ba6
mgKhantt <khantphonenaing156@gamil.com>
2024-05-12 19:19:16 +0630

    Adding 2 images and fix the control

--- script.js
+++ script.js
@@ -1,1 +1,13 @@
-console.log("Hello World");
+$(document).ready(()=> {
+    $('.nav-item').click((event) => {
+        $('.nav-item a').removeClass('active');
+        const element = $(event.currentTarget).children().eq(0);
+        element.addClass('active');
+    });
+
+    $('.quality').click((event) => {
+        $('.quality').removeClass('active');
+        const element = $(event.currentTarget).eq(0);
+        element.addClass('active');
+    });
+})
