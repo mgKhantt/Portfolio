7ddb0bb0d8d0fe2b8f36105f673f7e4b5f052f00
mgKhantt <khantphonenaing156@gamil.com>
2024-05-15 12:42:37 +0630

    Final Stage

--- script.js
+++ script.js
@@ -1,13 +1,31 @@
-$(document).ready(()=> {
-    $('.nav-item').click((event) => {
-        $('.nav-item a').removeClass('active');
+$(document).ready(() => {
+    $(".nav-item").click((event) => {
+        $(".nav-item a").removeClass("active");
         const element = $(event.currentTarget).children().eq(0);
-        element.addClass('active');
+        element.addClass("active");
     });
 
-    $('.quality').click((event) => {
-        $('.quality').removeClass('active');
+    $(".quality").click((event) => {
+        $(".quality").removeClass("active");
         const element = $(event.currentTarget).eq(0);
-        element.addClass('active');
+        element.addClass("active");
     });
-})
+
+    $(window).scroll(() => {
+        const winTop = $(window).scrollTop();
+
+        const enterPoint = winTop + 500;
+        const scrollBarPoint = winTop + 160;
+
+        $(".slideObject").each((index, obj) => {
+            const slideObject = $(obj);
+            const slideObjectPosition = slideObject.offset().top;
+
+            if (enterPoint > slideObjectPosition) {
+                slideObject.addClass(
+                    "animate__animated animate__bounceInRight"
+                );
+            }
+        });
+    });
+});
