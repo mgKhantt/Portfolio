fb111f7e23da69a771665bcc1de351084e7d057e
mgKhantt <khantphonenaing156@gamil.com>
2024-05-15 13:22:44 +0630

    v1

--- script.js
+++ script.js
@@ -10,22 +10,4 @@ $(document).ready(() => {
         const element = $(event.currentTarget).eq(0);
         element.addClass("active");
     });
-
-    $(window).scroll(() => {
-        const winTop = $(window).scrollTop();
-
-        const enterPoint = winTop + 500;
-        const scrollBarPoint = winTop + 160;
-
-        $(".slideObject").each((index, obj) => {
-            const slideObject = $(obj);
-            const slideObjectPosition = slideObject.offset().top;
-
-            if (enterPoint > slideObjectPosition) {
-                slideObject.addClass(
-                    "animate__animated animate__bounceInRight"
-                );
-            }
-        });
-    });
 });
